b73eac6bd7ddc76a8898cffa55c1ae9e3c017e68
Mr-React <ambureomkar@gmail.com>
2023-03-13 22:02:46 +0530

    configured the loader for css, scss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import generateJoke from "./generatejoke";
+import "./styles/main.scss";
 
 console.log("webpack-app");
 console.log(generateJoke());
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
